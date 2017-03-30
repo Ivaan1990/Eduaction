0a9f970dd43e0aa3220851f8d05ebe3b0a6b80e7
Ivaan1990 <ivaan7845@gmail.com>
2017-03-31 00:30:59 +0300

    Коряво увеличивает по спирали, НО! увеличивает

--- src/Java_zadachi/ExampleTester.java
+++ src/Java_zadachi/ExampleTester.java
@@ -7,12 +7,31 @@ public class ExampleTester {
 
     public static void main (String[] args) {
 
-        ExampleCool ob2 = new ExampleCool(12);
+        int[][] array = {
+
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0},
+
+        };
 
         out.println ("Original:");
-        ob2.showNewPrint();
-        out.println();
-        ob2.whach_Array_sum();
 
+        for (int i = 0; i < array.length; i ++) {
+            for (int j = 0; j < array[i].length; j ++) {
+
+                array[0][j] += j + 1;
+                array[i][2] += 1;
+                array[3][j] += 1;
+                array[2][0] += 1;
+
+                out.format ("%4d", array[i][j]);
+            }
+            out.println();
+
+        }
     }
 }
+
+
