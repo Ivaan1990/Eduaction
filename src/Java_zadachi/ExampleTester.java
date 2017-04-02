91179d4c555f810f856697127f7e3bfb069c655d
Ivaan1990 <ivaan7845@gmail.com>
2017-04-03 00:06:56 +0300

    Изменения от 03.04.2017 Корректно увеличивает, но не до конца.

--- src/Java_zadachi/ExampleTester.java
+++ src/Java_zadachi/ExampleTester.java
@@ -5,8 +5,8 @@ import static java.lang.System.out;
 
 public class ExampleTester {
 
-    public static void main (String[] args) {
-
+    public static void main(String[] args) {
+        boolean one = true;
         int[][] array = {
 
                 {0, 0, 0},
@@ -16,22 +16,33 @@ public class ExampleTester {
 
         };
 
-        out.println ("Original:");
+        out.println("Original:");
+        int count;
+        for (int i = 0; i < array.length; i++) {
 
-        for (int i = 0; i < array.length; i ++) {
-            for (int j = 0; j < array[i].length; j ++) {
+            int max = array[i][0];
+            int index = 0;
 
-                array[0][j] += j + 1;
-                array[i][2] += 1;
-                array[3][j] += 1;
-                array[2][0] += 1;
+            for (int j = 0; j < array[i].length; j++) {
 
-                out.format ("%4d", array[i][j]);
-            }
+                array[0][j] = j + 1;
+                array[i][2] = array[0][2] + i;
+                /*if (array[i][j] > max) {
+                    max = array[i][j];
+                    array[3][j] = max;
+                    index = j;
+                }
+                //array[3][index] = array[3][2] + j;
+                //array[3][j] = array[3][2] + j;
+                    //j = array[3][2]; array[1][2]
+                    //array[3][j] = j + 1;
+                    //array[2][0] += 1; */
+                    out.format("%4d", array[i][j]);
+                }
             out.println();
-
         }
     }
 }
+
 
 
