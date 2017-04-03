9e8d4366346ff77ffc4c70d4b9a8f88b6abd467c
Ivaan1990 <ivaan7845@gmail.com>
2017-04-04 00:42:21 +0300

    patch 04.0417

--- src/Java_zadachi/ExampleTester.java
+++ src/Java_zadachi/ExampleTester.java
@@ -17,29 +17,28 @@ public class ExampleTester {
         };
 
         out.println("Original:");
-        int count;
-        for (int i = 0; i < array.length; i++) {
+        try {
+            for (int i = 0; i < array.length; i++) {
 
-            int max = array[i][0];
-            int index = 0;
+                int max = array[i].length;
+                int index = 0;
 
-            for (int j = 0; j < array[i].length; j++) {
+                for (int j = 0; j < array[i].length; j++) {
 
-                array[0][j] = j + 1;
-                array[i][2] = array[0][2] + i;
-                /*if (array[i][j] > max) {
-                    max = array[i][j];
-                    array[3][j] = max;
-                    index = j;
-                }
-                //array[3][index] = array[3][2] + j;
-                //array[3][j] = array[3][2] + j;
-                    //j = array[3][2]; array[1][2]
-                    //array[3][j] = j + 1;
-                    //array[2][0] += 1; */
+                    array[0][j] = j + 1;
+                    array[i][2] = array[0][2] + i;
+                    array[3][j] = array[3][2] + j;
                     out.format("%4d", array[i][j]);
                 }
-            out.println();
+
+                int temp = array[max][index];
+                array[max][index] = array[max - 1 - i][index];
+                array[max - 1 - i][index] = temp;
+                out.println();
+
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            out.print("\nException IndexOUT");
         }
     }
 }
